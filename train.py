ab8d9ef4ad27f9d630887c83cb62c1ac8483b8ae
ngocnamk3er <ngocnamk3er@gmail.com>
2025-06-06 00:51:02 +0700

    change hubid and wandb id

--- train.py
+++ train.py
@@ -96,7 +96,7 @@ def main():
 
     # We use wandb to log Hits scores after each epoch. Note, this script does not save model checkpoints.
     wandb.login(key="c804f1ccb46b89fce13fb3bffe8b517ebb2ffc8a")
-    wandb.init(project="dsi_code_t5_base", name="dsi_code_t5_base_kaggle_hung")
+    wandb.init(project="dsi_code_t5_base", name="dsi_code_t5_base_kaggle_bach")
 
     tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir="cache")
     model = AutoModelForSeq2SeqLM.from_pretrained(model_name, cache_dir="cache")
@@ -163,7 +163,7 @@ def main():
         dataloader_num_workers=4,
         gradient_accumulation_steps=2,
         push_to_hub=True,
-        hub_model_id=f"ngocnamk3er/dsi_code_t5_base_kaggle_hung",
+        hub_model_id=f"ngocnamk3er/dsi_code_t5_base_kaggle_bach",
         hub_strategy="every_save",
     )
 
