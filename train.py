b60bd275b633d3a92cef89363a94bed5490435c6
ngocnamk3er <ngocnamk3er@gmail.com>
2025-06-05 12:14:27 +0700

    change train parameter

--- train.py
+++ train.py
@@ -151,7 +151,7 @@ def main():
         per_device_train_batch_size=64,
         per_device_eval_batch_size=64,
         evaluation_strategy="steps",
-        eval_steps=100,
+        eval_steps=2000,
         max_steps=10000,
         save_total_limit=1,
         dataloader_drop_last=False,  # necessary
