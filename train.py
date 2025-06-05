f786ee32bb0ca74042427d5d17a144f5d77b6c0b
ngocnamk3er <ngocnamk3er@gmail.com>
2025-06-05 11:05:53 +0700

    change dataset

--- train.py
+++ train.py
@@ -160,7 +160,7 @@ def main():
         save_strategy="steps",
         save_steps=1000,
         # fp16=True,  # gives 0/nan loss at some point during training, seems this is a transformers bug.
-        dataloader_num_workers=10,
+        dataloader_num_workers=4,
         gradient_accumulation_steps=2,
         push_to_hub=True,
         hub_model_id=f"ngocnamk3er/dsi_transformers_code_t5_base_python_kaggle",
